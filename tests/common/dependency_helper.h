29f4be8de28e847af1881e259efa18e41a817c4c
Saikat Dey <saikatdey@microsoft.com>
2023-11-21 15:12:08 +0530

    Convert tabs to spaces for consistency

--- tests/common/dependency_helper.h
+++ tests/common/dependency_helper.h
@@ -18,57 +18,57 @@ constexpr const wchar_t* VCLibsName = L"Microsoft.VCLibs.140.00_8wekyb3d8bbwe";
 
 struct PackageModel
 {
-	std::wstring pkg_name;
-	std::wstring pkg_version;
-	std::wstring pkg_publisher;
-	std::wstring pkg_install_path;
+    std::wstring pkg_name;
+    std::wstring pkg_version;
+    std::wstring pkg_publisher;
+    std::wstring pkg_install_path;
 };
 
 inline std::optional<PackageModel> query_package_with_name(std::wstring_view pkgFamilyName)
 {
-	auto currPkgArchitecture = Package::Current().Id().Architecture();
+    auto currPkgArchitecture = Package::Current().Id().Architecture();
 
     PackageManager pkgManager;
     
     Package currPkg = nullptr;
-	PackageVersion maxPkgVersion = { 0 };
+    PackageVersion maxPkgVersion = { 0 };
 
     IIterable<Package> possiblePackages = pkgManager.FindPackagesForUserWithPackageTypes(L"", pkgFamilyName, PackageTypes::Framework);
 
     for (auto&& pkg : possiblePackages)
     {
-		if (pkg.Id().Architecture() != currPkgArchitecture)
-		{
-			continue;
-		}
+        if (pkg.Id().Architecture() != currPkgArchitecture)
+        {
+            continue;
+        }
 
-		std::wstring pkg_name = pkg.Id().Name().c_str();
-		auto pkgVersion = pkg.Id().Version();
+        std::wstring pkg_name = pkg.Id().Name().c_str();
+        auto pkgVersion = pkg.Id().Version();
 
-		// If this is 1st candidate package, set currPkg and maxPkgVersion to this package
-		if (currPkg == nullptr)
-		{
-			currPkg = pkg;
-			maxPkgVersion = pkgVersion;
-		}
-		// For others, compare version and set currPkg and maxPkgVersion to the package with highest version
-		else
-		{
-			if (maxPkgVersion.Major < pkgVersion.Major ||
-				(maxPkgVersion.Major == pkgVersion.Major && maxPkgVersion.Minor < pkgVersion.Minor) ||
-				(maxPkgVersion.Major == pkgVersion.Major && maxPkgVersion.Minor == pkgVersion.Minor && maxPkgVersion.Build < pkgVersion.Build) ||
-				(maxPkgVersion.Major == pkgVersion.Major && maxPkgVersion.Minor == pkgVersion.Minor && maxPkgVersion.Build == pkgVersion.Build && maxPkgVersion.Revision < pkgVersion.Revision))
-			{
-				maxPkgVersion = pkgVersion;
-				currPkg = pkg;
-			}
-		}
-	}
+        // If this is 1st candidate package, set currPkg and maxPkgVersion to this package
+        if (currPkg == nullptr)
+        {
+            currPkg = pkg;
+            maxPkgVersion = pkgVersion;
+        }
+        // For others, compare version and set currPkg and maxPkgVersion to the package with highest version
+        else
+        {
+            if (maxPkgVersion.Major < pkgVersion.Major ||
+                (maxPkgVersion.Major == pkgVersion.Major && maxPkgVersion.Minor < pkgVersion.Minor) ||
+                (maxPkgVersion.Major == pkgVersion.Major && maxPkgVersion.Minor == pkgVersion.Minor && maxPkgVersion.Build < pkgVersion.Build) ||
+                (maxPkgVersion.Major == pkgVersion.Major && maxPkgVersion.Minor == pkgVersion.Minor && maxPkgVersion.Build == pkgVersion.Build && maxPkgVersion.Revision < pkgVersion.Revision))
+            {
+                maxPkgVersion = pkgVersion;
+                currPkg = pkg;
+            }
+        }
+    }
 
-	if (!currPkg)
+    if (!currPkg)
     {
-		return std::nullopt;
-	}
+        return std::nullopt;
+    }
 
     PackageModel model;
     model.pkg_name = currPkg.Id().Name().c_str();
